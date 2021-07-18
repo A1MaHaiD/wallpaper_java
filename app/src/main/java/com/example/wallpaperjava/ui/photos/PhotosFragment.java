2a2dbe7b7ff80e3e27f1d379312bf63a77b0b042
HanDroiD <super.lukianchuk2010@gmail.com>
2021-07-18 03:19:06 +0300

    remove <com.google.android.material.floatingactionbutton.FloatingActionButton/> && add <uses-permission android:name="android.permission.INTERNET"/> . Changed ApiInterface && PhotosFragment && I have @maybe something wrong

--- app/src/main/java/com/example/wallpaperjava/ui/photos/PhotosFragment.java
+++ app/src/main/java/com/example/wallpaperjava/ui/photos/PhotosFragment.java
@@ -8,12 +8,32 @@ import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.wallpaperjava.R;
+import com.example.wallpaperjava.adapters.PhotosAdapter;
 import com.example.wallpaperjava.databinding.FragmentPhotosBinding;
+import com.example.wallpaperjava.models.Photo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class PhotosFragment extends Fragment {
+    private final String TAG = PhotosFragment.class.getSimpleName();
+    @BindView(R.id.pb_fragment_photos)
+    ProgressBar progressBar;
+    @BindView(R.id.rv_fragment_photos)
+    RecyclerView recyclerView;
 
+    private PhotosAdapter photosAdapter;
+    private List<Photo> photos  = new ArrayList<>();
+
+    private Unbinder unbinder;
     private FragmentPhotosBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -21,19 +41,28 @@ public class PhotosFragment extends Fragment {
 
 
         binding = FragmentPhotosBinding.inflate(inflater, container, false);
+        //@maybe something wrong
+        unbinder = ButterKnife.bind(this, binding.getRoot());
         View root = binding.getRoot();
+//        //@maybe something wrong
+//        //added progressBar && recyclerView
+//        final RecyclerView recyclerView = binding.rvFragmentPhotos;
+//        final ProgressBar progressBar = binding.pbFragmentPhotos;
+        //@recyclerView
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
 
-        //added progressBar && recyclerView
-        final RecyclerView recyclerView = binding.rvFragmentPhotos;
-        final ProgressBar progressBar = binding.pbFragmentPhotos;
-
-
+        photosAdapter = new PhotosAdapter(getActivity(),photos);
+        recyclerView.setAdapter(photosAdapter);
         return root;
     }
+
+    //todo need add @getPhoto function
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        unbinder.unbind();
         binding = null;
     }
 }
