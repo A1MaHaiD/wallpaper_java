58fe2331c1eae2229d92ce64c484e8e04ad239e8
HanDroiD <super.lukianchuk2010@gmail.com>
2021-07-19 14:45:10 +0300

    changed fragment_favorite.xml && FavoriteFragment.java && deleted FavoriteViewModel.java This is last commit. App completed

--- app/src/main/java/com/example/wallpaperjava/ui/favorite/FavoriteFragment.java
+++ app/src/main/java/com/example/wallpaperjava/ui/favorite/FavoriteFragment.java
@@ -11,35 +11,67 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.wallpaperjava.R;
+import com.example.wallpaperjava.adapters.PhotosAdapter;
 import com.example.wallpaperjava.databinding.FragmentFavoriteBinding;
+import com.example.wallpaperjava.models.Photo;
+import com.example.wallpaperjava.utils.RealmController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class FavoriteFragment extends Fragment {
 
-    private FavoriteViewModel favoriteViewModel;
+    @BindView(R.id.rv_fragment_favorite)
+    RecyclerView recyclerView;
+    @BindView(R.id.tv_fragment_favorite_notification)
+    TextView notification;
+
+    private PhotosAdapter photosAdapter;
+    private List<Photo> photos = new ArrayList<>();
+    private Unbinder unbinder;
+
     private FragmentFavoriteBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        favoriteViewModel =
-                new ViewModelProvider(this).get(FavoriteViewModel.class);
 
         binding = FragmentFavoriteBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        unbinder = ButterKnife.bind(this,root);
 
-        final TextView textView = binding.textFavorite;
-        favoriteViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        //@recyclerView
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        photosAdapter = new PhotosAdapter(getActivity(), photos);
+        recyclerView.setAdapter(photosAdapter);
+        getPhotos();
+
         return root;
+    }
+
+    private void getPhotos(){
+        RealmController realmController = new RealmController();
+        photos.addAll(realmController.getPhotos());
+        if (photos.size() == 0){
+            notification.setVisibility(View.VISIBLE);
+            recyclerView.setVisibility(View.GONE);
+        } else {
+            photosAdapter.notifyDataSetChanged();
+        }
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        unbinder.unbind();
         binding = null;
     }
 }
